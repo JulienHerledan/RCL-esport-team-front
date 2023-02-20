4a28c88fe501c2ee3ef5cca09e4a13ea3c2f4bb4
Etudiant O'clock <julien.herledan@gmail.com>
2023-02-20 14:58:56 +0100

    build: create Article entity with comments

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -33,6 +33,12 @@ class Comment
      */
     private $updatedAt;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Article::class, inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $article;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -70,6 +76,18 @@ class Comment
     public function setUpdatedAt(?\DateTimeImmutable $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getArticle(): ?Article
+    {
+        return $this->article;
+    }
+
+    public function setArticle(?Article $article): self
+    {
+        $this->article = $article;
 
         return $this;
     }
