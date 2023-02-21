1a6c8865d50a49c95d00dba39a16b449ad6429f6
Jim Dupuy <dupuy.jim@gmail.com>
2023-02-21 11:15:32 +0100

    build(api):videoclip-route-controller

---
+++ src/Controller/API/VideoClipController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\API;
+
+use App\Repository\VideoClipRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VideoClipController extends AbstractController
+{
+    /**
+     * @Route("/api/video-clip", name="app_api_video_clip_getAll", methods={"GET"})
+     */
+    public function getAll(VideoClipRepository $videoClipRepository): Response
+    {   
+        $videos = $videoClipRepository->findAll();
+        return $this->json($videos, Response::HTTP_OK);
+    }
+
+
+}
+
