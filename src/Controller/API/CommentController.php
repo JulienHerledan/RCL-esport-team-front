8ad46b780b4cf3c9e42547cb9258cb3ec0cda9b1
Jim Dupuy <dupuy.jim@gmail.com>
2023-02-27 14:40:16 +0100

    add(feat):associate and show authors on comments

--- src/Controller/API/CommentController.php
+++ src/Controller/API/CommentController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\API;
 
+use App\Entity\User;
 use App\Entity\Comment;
 use App\Repository\CommentRepository;
+use Faker\Guesser\Name;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,24 +17,27 @@ use Symfony\Component\HttpFoundation\Request;
 
 class CommentController extends AbstractController
 {
-
-
   /**
    *
    * @Route("/api/comments", name="app_api_comment_create", methods={"POST"})
+   * 
    */
-  public function create(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, CommentRepository $commentRepository): Response
+   public function create(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, CommentRepository $commentRepository): Response
   {
     // Get json content from the request
     $json = $request->getContent();
-
+    
+    
     // Deserialize our object
     try {
       $comment = $serializer->deserialize($json, Comment::class, 'json');
+      $user = $this->getUser();
+      $comment->setAuthor($user);
     } catch (NotEncodableValueException $e) {
+      
       return $this->json(["error" => "Json non valide"], Response::HTTP_BAD_REQUEST);
     }
-
+    
     // Check errors in values
     $errors = $validator->validate($comment);
 
@@ -46,10 +51,13 @@ class CommentController extends AbstractController
 
     // Save object in database
     $commentRepository->add($comment, true);
-
+    
     // Return the object created
     // Don't return the location because we don't need a route to get one comment
+    
     return $this->json($comment, Response::HTTP_CREATED, [], ["groups" => "comments"]);
+    
+    
   }
 }
 
