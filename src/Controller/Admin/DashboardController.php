c946bdddc7d3ca5589dc3addd686614990db25a3
Etudiant O'clock <julien.herledan@gmail.com>
2023-02-28 13:39:41 +0100

    configure: Menu Items

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -29,6 +29,7 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         return parent::index();
+        // return $this->render('some/path/my-dashboard.html.twig');
 
         // redirect to some CRUD controller
         // $routeBuilder = $this->get(AdminUrlGenerator::class);
@@ -44,23 +45,29 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        
-        yield MenuItem::linkToCrud('User', 'fas fa-users', User::class);
+        yield MenuItem::section('Users');
+        yield MenuItem::linkToCrud('Users', 'fas fa-users', User::class);
+
+        yield MenuItem::section('Articles');
         yield MenuItem::linkToCrud('Articles', 'far fa-newspaper', Article::class);
-        yield MenuItem::linkToCrud('Comment', 'far fa-comments', Comment::class);
-        yield MenuItem::linkToCrud('Apply', 'fas fa-id-card-alt', Apply::class);
-        yield MenuItem::linkToCrud('Member', 'fas fa-headset', Member::class);
-        yield MenuItem::linkToCrud('Game', 'fas fa-gamepad', Game::class);
-        yield MenuItem::linkToCrud('SocialNetwork', 'fab fa-twitter-square', SocialNetwork::class);
-        yield MenuItem::linkToCrud('SocialNetwork link', 'fas fa-link', SocialNetworkLink::class);
-        yield MenuItem::linkToCrud('videoClip', 'fas fa-video', VideoClip::class);
-        yield MenuItem::linkToCrud('Award', 'fas fa-trophy', Award::class);
-        yield MenuItem::linkToCrud('Competition', 'fas fa-calendar-week', Competition::class);
-        yield MenuItem::linkToCrud('Matche', 'fas fa-sitemap', Matche::class);
+        yield MenuItem::linkToCrud('Comments', 'far fa-comments', Comment::class);
+
+        yield MenuItem::section('Apply');
+        yield MenuItem::linkToCrud('Applys', 'fas fa-id-card-alt', Apply::class);
+
+        yield MenuItem::section('Members');
+        yield MenuItem::linkToCrud('Members', 'fas fa-headset', Member::class);
+        yield MenuItem::linkToCrud('Games', 'fas fa-gamepad', Game::class);
+        yield MenuItem::linkToCrud('SocialNetworks', 'fab fa-twitter-square', SocialNetwork::class);
+        yield MenuItem::linkToCrud('SocialNetwork links', 'fas fa-link', SocialNetworkLink::class);
+        yield MenuItem::linkToCrud('VideoClips', 'fas fa-video', VideoClip::class);
+        yield MenuItem::linkToCrud('Awards', 'fas fa-trophy', Award::class);
+        yield MenuItem::linkToCrud('Competitions', 'fas fa-calendar-week', Competition::class);
+        yield MenuItem::linkToCrud('Matches', 'fas fa-sitemap', Matche::class);
 
         //@todo rajouter le lien vers le font office
         // ==> MenuItem::linkToUrl
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
-        
+
     }
 }
