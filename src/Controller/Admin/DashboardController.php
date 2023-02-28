6ce79070a7eb71f0e8b8707e218740b1fe72764f
Etudiant O'clock <julien.herledan@gmail.com>
2023-02-28 13:20:20 +0100

    bugfix: SocialNetworkLink targetentity=Member

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -17,6 +17,7 @@ use App\Entity\VideoClip;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,19 +29,23 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         return parent::index();
+
+        // redirect to some CRUD controller
+        // $routeBuilder = $this->get(AdminUrlGenerator::class);
+
+        // return $this->redirect($routeBuilder->setController(UserCrudController::class)->generateUrl())
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Projet 14 Rcl Esport Team Back');
+            ->setTitle('Rcl Esport Team Back');
     }
 
     public function configureMenuItems(): iterable
     {
         
         yield MenuItem::linkToCrud('User', 'fas fa-users', User::class);
-
         yield MenuItem::linkToCrud('Articles', 'far fa-newspaper', Article::class);
         yield MenuItem::linkToCrud('Comment', 'far fa-comments', Comment::class);
         yield MenuItem::linkToCrud('Apply', 'fas fa-id-card-alt', Apply::class);
@@ -53,10 +58,9 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Competition', 'fas fa-calendar-week', Competition::class);
         yield MenuItem::linkToCrud('Matche', 'fas fa-sitemap', Matche::class);
 
-        
-
         //@todo rajouter le lien vers le font office
         // ==> MenuItem::linkToUrl
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        
     }
 }
