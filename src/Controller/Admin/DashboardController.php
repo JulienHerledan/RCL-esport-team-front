95328732a3fbf595b8a2b3906a7f8469fbbfc74e
Etudiant O'clock <julien.herledan@gmail.com>
2023-02-28 11:41:02 +0100

    fix: link to crud for comment

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Apply;
 use App\Entity\Article;
+use App\Entity\Award;
+use App\Entity\Comment;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -32,7 +34,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('User', 'fas fa-users');
 
         yield MenuItem::linkToCrud('Articles', 'far fa-newspaper', Article::class);
-        yield MenuItem::linkToDashboard('Comment', 'far fa-comments');
+        yield MenuItem::linkToCrud('Comment', 'far fa-comments', Comment::class);
         yield MenuItem::linkToCrud('Apply', 'fas fa-id-card-alt', Apply::class);
         yield MenuItem::linkToDashboard('Member', 'fas fa-headset');
         yield MenuItem::linkToDashboard('Game', 'fas fa-gamepad');
